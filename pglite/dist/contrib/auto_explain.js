1bf5da15350185b66e9b21799c73b78752a88fe0
pmp-p <mail.peny@free.fr>
2024-08-12 12:57:51 +0000

    Deploying to gh-pages from @ pmp-p/pglite-16.x@8c574199fb97947c4a0fc21df0d6ed091d0c9229 🚀

--- pglite/dist/contrib/auto_explain.js
+++
@@ -1,2 +0,0 @@
-import{i as e}from"../chunk-ZYB3MGPW.js";e();var t=async(n,s)=>({bundlePath:new URL("..//auto_explain.tar.gz",import.meta.url)}),o={name:"auto_explain",setup:t};export{o as auto_explain};
-//# sourceMappingURL=auto_explain.js.map
--- pglite/dist/vector/index.js
+++
@@ -1,2 +0,0 @@
-import{i as e}from"../chunk-ZYB3MGPW.js";e();var n=async(s,t)=>({emscriptenOpts:t,bundlePath:new URL("..//vector.tar.gz",import.meta.url)}),o={name:"pgvector",setup:n};export{o as vector};
-//# sourceMappingURL=index.js.map
