8ef21c0713472a162b075247e805fd9ba38f1bf1
pmp-p <mail.peny@free.fr>
2024-08-17 12:07:37 +0000

    Deploying to gh-pages from @ pmp-p/pglite-16.x@46607485e105441772cf6017c428f050bd18e0d3 🚀

--- pglite/dist/contrib/cube.js
+++
@@ -1,2 +0,0 @@
-import{i as e}from"../chunk-FRCO5TGY.js";e();var t=async(n,s)=>({bundlePath:new URL("..//cube.tar.gz",import.meta.url)}),a={name:"cube",setup:t};export{a as cube};
-//# sourceMappingURL=cube.js.map
