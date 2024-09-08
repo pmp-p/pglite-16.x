ba045bf53fed3c393220b5659344e7c6b3d4147e
pmp-p <mail.peny@free.fr>
2024-09-08 11:08:36 +0200

    more wasi

--- patches/pg_main.c
+++ patches/pg_main.c
@@ -1169,7 +1169,34 @@ __SIG_IGN(int) {
     #define icu_validate_locale(loc_str) icu_validate_locale_idb(loc_str)
     #include "../postgresql/src/interfaces/libpq/pqexpbuffer.c"
     #define fsync_pgdata(...)
+
+    #include <stdio.h> // FILE+fprintf
+    extern FILE* IDB_PIPE_FP;
+    extern FILE* SOCKET_FILE;
+    extern int SOCKET_DATA;
+    extern int IDB_STAGE;
+    FILE *pg_popen(const char *command, const char *type) {
+        if (IDB_STAGE>1) {
+        	fprintf(stderr,"# popen[%s]\n", command);
+        	return stderr;
+        }
+
+        if (!IDB_STAGE) {
+            fprintf(stderr,"# popen[%s] (BOOT)\n", command);
+            IDB_PIPE_FP = fopen( IDB_PIPE_BOOT, "w");
+            IDB_STAGE = 1;
+        } else {
+            fprintf(stderr,"# popen[%s] (SINGLE)\n", command);
+            IDB_PIPE_FP = fopen( IDB_PIPE_SINGLE, "w");
+            IDB_STAGE = 2;
+        }
+
+        return IDB_PIPE_FP;
+
+    }
+
     #include "../postgresql/src/bin/initdb/initdb.c"
+
 
 #undef PG_INITDB_MAIN
 #undef PG_MAIN
