cd2f98c96d147275670f7d857b4949cea2a5fa52
pmp-p <mail.peny@free.fr>
2024-09-12 16:50:06 +0000

    Deploying to gh-pages from @ pmp-p/pglite-16.x@8f516c55da94ebd19f59486d004ec0d3b3576525 🚀

--- postgres.js
+++ postgres.js
@@ -53,7 +53,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpp8x45__2.js
+// include: /tmp/tmpyq7ln6zl.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -279,7 +279,7 @@ Module['FS_createPath']("/tmp/pglite/share/postgresql", "tsearch_data", true, tr
 
   })();
 
-// end include: /tmp/tmpp8x45__2.js
+// end include: /tmp/tmpyq7ln6zl.js
 
 
 // Sometimes an existing Module object exists with properties
