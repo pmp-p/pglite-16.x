58a21ef1215355392275915efba20bef71054c1f
pmp-p <mail.peny@free.fr>
2024-09-24 20:56:27 +0200

    (maybe) fix shm_resize, some kill sig, bump sdk

--- tests/pglite-wasi.py
+++ tests/pglite-wasi.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
-
+import sys
 import asyncio
+
+try:
+    import wasmtime
+except:
+    import os
+    os.system(f'{sys.executable} -m pip install --user wasmtime')
+    __import__('importlib').invalidate_caches()
+
+with __import__('tarfile').open('pglite-wasi.tar.gz') as archive:
+    archive.extractall(".")
+
+
+
 
 class wasm_import:
     import os
@@ -20,7 +33,7 @@ class wasm_import:
             call = self.instance.exports(self.store)[export]
             store = self.store
             def bound(*argv, **env):
-                return call(store)
+                return call(store, *argv, **env)
             return bound
 
         def __all__(self):
@@ -44,7 +57,14 @@ class wasm_import:
     config.inherit_stdout()
     config.inherit_stderr()
 
-    config.preopen_dir('/tmp', '/tmp')
+
+
+    config.preopen_dir('tmp', '/tmp')
+    if not os.path.isdir("dev"):
+        os.mkdir("dev")
+    with open("dev/urandom","wb") as rng_out:
+        rng_out.write( os.urandom(128) );
+    config.preopen_dir('dev', '/dev')
 
     linker = Linker(Engine())
     # linker.allow_shadowing = True
@@ -68,16 +88,19 @@ class wasm_import:
                 self.mem = mem
                 self.mod = wasm_mod
 
+            def mpoke(self, addr, b):
+                return self.mod.mem.write(self.mod.store, b, addr)
+
+            def mpeek(self, addr, stop:None):
+                return self.mod.mem.read(self.mod.store, addr, stop)
+
+
             def __getattr__(self, attr):
                 if attr=='size':
                     return self.mod.mem.size(self.mod.store)
                 if attr=='data_len':
                     return self.mod.mem.data_len(self.mod.store)
                 return object.__getattr__(self, attr)
-
-#        def getter(attr):
-#            return wasm_mod.get(attr)
-#        py_mod.__getattr__ = getter
 
         for k,v in wasm_mod.instance.exports(wasm_mod.store).items():
             if k=='memory':
@@ -121,19 +144,70 @@ print(f"""
 initdb returned : {bin(rv)}
 
 {SI(pglite.Memory.size)=}
-{SI(pglite.Memory.data_len)=}
+
+{SI(pglite.Memory.data_len)=} <= with included 32 MiB shared memory
 
 {pglite=}
 """)
 for k in dir(pglite):
     print("\t", k)
 
+
+TESTS="""
+
+SHOW client_encoding;
+
+CREATE OR REPLACE FUNCTION test_func() RETURNS TEXT AS $$ BEGIN RETURN 'test'; END; $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION addition (entier1 integer, entier2 integer)
+RETURNS integer
+LANGUAGE plpgsql
+IMMUTABLE
+AS '
+DECLARE
+  resultat integer;
+BEGIN
+  resultat := entier1 + entier2;
+  RETURN resultat;
+END ' ;
+
+SELECT test_func();
+
+SELECT now(), current_database(), session_user, current_user;
+
+SELECT addition(40,2);
+
+"""
+
+DONE = 0
+
+async def tests():
+    global DONE
+    for line in TESTS.split(";\n\n"):
+        await asyncio.sleep(2)
+
+        if line.strip():
+            line = line.strip()+";"
+            print(f"REPL: {line}")
+            sql_bytes_cstring = line.encode('utf-8')+ b"\0" # <- do not forget this one, wasmtime won't add anything!
+            pglite.Memory.mpoke(1, sql_bytes_cstring)
+
+    await asyncio.sleep(2)
+    DONE = 1
+
 async def main():
+    global DONE
     pglite.use_socketfile()
+
+    asyncio.get_running_loop().create_task(tests())
+    i = 0
     while True:
+        if DONE:
+            break
         pglite.interactive_one()
         await asyncio.sleep(0.016)
 
+    print("bye")
 
 asyncio.run(main())
 
